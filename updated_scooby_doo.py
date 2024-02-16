f694f6d6764250e134565da661cd03706839c19f
Michael Olmstead <molmstead4876@xmail.dixietech.edu>
2024-02-16 07:27:57 -0800

    Changed move function and a couple menu options

--- updated_scooby_doo.py
+++ updated_scooby_doo.py
@@ -2,34 +2,10 @@ from abc import ABC, abstractmethod
 
 import random
 
-# Location info
-locations = {
-    "gravesite": 0,
-    "g": 0,
-    "masoleum": 1,
-    "m": 1,
-    "chapel": 2,
-    "c": 2,
-    "gardens": 3,
-    "gd": 3,
-    "entrance": 4,
-    "e": 4
-}
-user_location = locations["entrance"]
-mystery_gang_location = locations["entrance"]
-# Clue info
-player_clues = []
-clues = ["Strange Note", "Shovel", "Footprints", "Broken headstone", "Torn Cloth"]
-clue_locations = {
-    locations["gravesite"]: clues[3],
-    locations["masoleum"]: clues[4],
-    locations["chapel"]: clues[0],
-    locations["gardens"]: clues[1],
-    locations["entrance"]: clues[2]
-}
-turn_count = 0
+import time
 
-class Player(ABC):
+
+class NPC(ABC):
     def __init__(self, name: str, mood: str):
         self.name = name
         self.mood = mood
@@ -40,95 +16,125 @@ class Player(ABC):
         pass
     
     def __str__(self):
-        return f"Name: {self.name}\nMood: {self.mood}\n"
+        return f"\nMood: {self.mood}\n"
     
-    def move_location(user_location, turn_count):
+
+class Player:
+    
+    
+    def move_location(self, user_location: str) -> str:
         valid_input = False
-        turn_count += 1
+        
         while not valid_input:
-            move = input("\nWhere would you like to move? Gravesite (g), Mausoleum (m), Chapel (c), Gardens (gd), Entrance (e): ").lower()
+            move = input("\nWhere would you like to move? Gravesite (g): Mausoleum (m): Chapel (c): Gardens (gd): Entrance (e): ").lower()
             if move == user_location:
                 print("You're already here.")
             elif move in locations:
                 user_location = locations[move]
+                print(f"\nYou decide to go to the {user_location}.")
                 valid_input = True
-            else:
-                print("You can't go there. Try somewhere else.")
-        return user_location, turn_count
-    
-    def find_clues(user_location, player_clues, turn_count):
-        # Simulate finding clues based on the location
-        turn_count += 1
-        user_search = random.randint(1, 3)
-        if user_search == 3:
-            print("You see nothing out of the ordinary.")
-        else:
-            player_clues.append(clue_locations.get(user_location, ""))
-    
-    def search_location(user_location):
-        interactive_objects = {}
+                
+        return user_location
 
+    
+    def search_location(self, user_location):
+        interactive_objects = locations[user_location]
         for location, object in locations.items():
             # Define interactive objects based on location code
             if object == 0:  # Gravesite
                 interactive_objects[location] = ["headstone", "funeral urn", "dead flowers", "lantern"]
+                print("")
             elif object == 1:  # Mausoleum
                 interactive_objects[location] = ["crypt", "statue", "torch", "cobwebs"]
+                print("")
             elif object == 2:  # Chapel
                 interactive_objects[location] = ["pew", "altar", "stained glass window"]
+                print("")
             elif object == 3:  # Gardens
                 interactive_objects[location] = ["fountain", "decrepit bench", "flower bed", "bird feeder"]
+                print("")
             elif object == 4:  # Entrance
                 interactive_objects[location] = ["gate", "signboard", "gargoyle statue", "lamp post"]
+                print("")
 
-        return interactive_objects
+        return interactive_objects, turn_count
+        
+        
+    def interact_with_object(self, user_location):
+        pass    
     
-    def look_at_clues():
+    
+    def look_at_clues(self, player_clues):
+        if not player_clues:
+            print("You don't have any clues yet.")
         for clue in player_clues:
-            print(f"\nCurrent clues: {clue}")
+            print(f"- {clue}")
+            
         
-        
-class Fred(Player):
+class Fred(NPC):
     def __init__(self):
         super().__init__(name="Fred", mood="confident")
     def take_action(self):
-        print("Fred: Alright Gang, let's split up and look for clues. Daphne, Velma, you're with me. ")
+        print("\nAlright Gang, let's split up and look for clues. Daphne, Velma, you're with me. ")
 
 
-class Daphne(Player):
+class Daphne(NPC):
     def __init__(self):
         super().__init__(name="Daphne", mood="curious")
     def take_action(self):
-        print("Daphne: I'll search the gravesites for any signs of the ghost. ")
+        print("\nI'll search the gravesites for any signs of the ghost. ")
 
 
-class Velma(Player):
+class Velma(NPC):
     def __init__(self):
         super().__init__(name="Velma", mood="analytical")
     def take_action(self):
-        print("Velma: I'll investigate the mausoleum for any clues.")
+        print("\nI'll investigate the mausoleum for any clues.")
 
 
-class Shaggy(Player):
+class Shaggy(NPC):
     def __init__(self):
         super().__init__(name="Shaggy", mood="hungry")
     def take_action(self):
-        print("Shaggy: Like, I'll check out the entrance area. Maybe there's some food around. ")
+        print("\nLike, I'll check out the entrance area. Maybe there's some food around. ")
 
 
-class Scooby(Player):
+class Scooby(NPC):
     def __init__(self):
         super().__init__(name="Scooby", mood="nervous")
     def take_action(self):
-        print("Scooby: Ruh-roh! I'll join Shaggy in the entrance. ")
+        print("\nRuh-roh! I'll join Shaggy in the entrance. ")
 
 
-class Ghost(Player):
+class Ghost(NPC):
     def __init__(self):
         super().__init__(name="Ghost", mood="upset")
     def take_action(self):
-        print("Ghost: Wooooo! You meddling kids will never solve the mystery! ")
+        print("\nWooooo! You meddling kids will never solve the mystery! ")
 
+
+# Game info
+locations = {
+    "g": "gravesite",
+    "m": "mausoleum",
+    "c": "chapel",
+    "gd": "gardens",
+    "e": "entrance"
+}
+
+user_location = "entrance"
+
+clue_locations = {
+    "gravesite": [("headstone", "broken headstone")],
+    "mausoleum": [("crypt", "torn cloth")],
+    "chapel": [("altar", "strange note")],
+    "gardens": [("flower bed", "shovel")],
+    "entrance": [("dirt pathway", "footprints")]
+}
+
+player_clues = []
+
+turn_count = 0
 
 characters = {
     "Fred": Fred(),
@@ -140,7 +146,7 @@ characters = {
 }
 
 def main():
-    
+    player = Player()
     
     #Introduction
     print()
@@ -151,53 +157,34 @@ def main():
         Daphne added you to the group chat that shows the current location of each gang member
         and what they plan to do. """)
     print()
+    #time.sleep(5)
     
     # Print character name, location, mood, and take action
     for character_name, character in characters.items():
-        print(f"{character_name}:\n{str(character)}")
+        #time.sleep(5)
+        print(f"\n{character_name}:") 
         character.take_action()
-        print()
+        print(f"{str(character)}")
+        
+    turn_count = 0
     while turn_count < 15:
-        print(f"\nCurrent location: {user_location}. Mystery gang locations: {mystery_gang_locations}")
+        turn_count += 1
+        
+        print(f"\nYou are currently in the {user_location}.")
         try:
-            user_input = input("\nWhat would you like to do? Move location: (m), Search current location: (s), Interact with object: (i), Look for clues: (c), Look at your list of clues: (l): ").lower
+            user_input = input("\nWhat would you like to do? Move location: (m), Search current location: (s), Interact with object: (i), Look at your list of clues: (l): ").lower()
             if user_input == "m" or user_input == "move":
-                player.move_location()
-            elif user_input == "s":
+                player.move_location(user_location)
+            elif user_input == "s" or user_input == "search":
                 player.search_location()            
-            elif user_input == "i":
-                player.interact_with_object
-            elif user_input == "c":
-                player.find_clues()
+            elif user_input == "i" or user_input == "interact":
+                player.interact_with_object()
             elif user_input == "l":
                 player.look_at_clues()
+                
+        except ValueError:
+            print("\nThat's not valid input.")
 
-# Location info
-locations = {
-    "gravesite": 0,
-    "g": 0,
-    "masoleum": 1,
-    "m": 1,
-    "chapel": 2,
-    "c": 2,
-    "gardens": 3,
-    "gd": 3,
-    "entrance": 4,
-    "e": 4
-}
-user_location = locations["entrance"]
-mystery_gang_location = locations["entrance"]
-# Clue info
-player_clues = []
-clues = ["Strange Note", "Shovel", "Footprints", "Broken headstone", "Torn Cloth"]
-clue_locations = {
-    locations["gravesite"]: clues[3],
-    locations["masoleum"]: clues[4],
-    locations["chapel"]: clues[0],
-    locations["gardens"]: clues[1],
-    locations["entrance"]: clues[2]
-}
-turn_count = 0
 
 if __name__ == "__main__":
     main()
